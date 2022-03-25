8401602935c72987279e03b4be817c27fc1221f4
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-03-25 16:09:46 -0600

    First iteration of redis

--- src/routes/aws.js
+++ src/routes/aws.js
@@ -3,40 +3,95 @@ const express = require("express");
 const multiparty = require("multiparty");
 const { GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+const redis = require("../lib/redis");
 const { s3Client } = require("../lib/aws");
 const { streamToBase64 } = require("../helpers/utils");
 
 const router = express.Router();
 
-router.post("/get", async (req, res) => {
+router.post("/get-albums", async (req, res) => {
   try {
-    const { folder, file } = req.body;
+    const { user } = req.body;
 
-    if (!req.body || !folder || !file) {
+    if (!req.body || !user) {
+      throw new Error("No user");
+    }
+
+    const albums = await redis.get(user);
+
+    if (!albums) {
+      return res.json({ albums: [] });
+    }
+
+    const parsed = JSON.parse(albums);
+
+    return res.json({ albums: Object.keys(parsed) });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/get-album", async (req, res) => {
+  try {
+    const { user, album } = req.body;
+
+    if (!req.body || !user || !album) {
       throw new Error("No body");
     }
 
-    const response = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: "slingercode-test-1",
-        Key: `${folder}/${file}`,
-      })
-    );
+    const albums = await redis.get(user);
 
-    const base64 = await streamToBase64(response.Body);
+    if (!albums) {
+      throw new Error("No albums");
+    }
 
-    res.writeHead(200, {
-      "Content-Type": "img/webp",
-      "Content-Length": base64.length,
-    });
+    const parsed = JSON.parse(albums);
 
-    return res.end(base64);
+    return res.json({ data: parsed[album] });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/create-album", async (req, res) => {
+  try {
+    const { user, album } = req.body;
+
+    if (!req.body || !user || !album) {
+      throw new Error("No body");
+    }
+
+    const dbUser = await redis.get(user);
+
+    if (!dbUser) {
+      await redis.set(user, JSON.stringify({ [album]: [] }));
+      return res.json({ album });
+    }
+
+    let parsed = JSON.parse(dbUser);
+    const albums = Object.keys(parsed);
+
+    if (albums.some((_album) => _album === album)) {
+      throw new Error("This album already exists");
+    }
+
+    parsed = { ...parsed, [album]: [] };
+
+    await redis.set(user, JSON.stringify(parsed));
+
+    return res.json({ album });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
 
 router.post("/upload", (req, res) => {
+  const { user, album } = req.query;
+
+  if (!req.query || !user || !album) {
+    return res.status(500).json({ error: "No query" });
+  }
+
   let filename = "";
   const chunks = [];
   const form = new multiparty.Form();
@@ -65,20 +120,35 @@ router.post("/upload", (req, res) => {
       .toBuffer()
       .then(async (blob) => {
         try {
-          const bodyContents = blob.toString("base64");
+          const albums = await redis.get(user);
 
-          const data = await s3Client.send(
+          if (!albums) {
+            throw new Error("No albums");
+          }
+
+          let parsed = JSON.parse(albums);
+          const selectedAlbum = parsed[album];
+
+          if (selectedAlbum.some((name) => name.includes(filename))) {
+            throw new Error("Same name");
+          }
+
+          parsed = {
+            ...parsed,
+            [album]: [...parsed[album], `${filename}.webp`],
+          };
+
+          await redis.set(user, JSON.stringify(parsed));
+
+          await s3Client.send(
             new PutObjectCommand({
               Bucket: "slingercode-test-1",
-              Key: `noel/${filename}.webp`,
+              Key: `${user}/${album}/${filename}.webp`,
               Body: blob,
             })
           );
 
-          return res.status(200).json({
-            data,
-            bodyContents,
-          });
+          return res.status(200).json({ newFile: `${filename}.webp` });
         } catch (error) {
           return res.status(500).json({ error: error.message });
         }
@@ -87,6 +157,34 @@ router.post("/upload", (req, res) => {
   });
 
   form.parse(req);
+});
+
+router.post("/get", async (req, res) => {
+  try {
+    const { user, album, file } = req.body;
+
+    if (!req.body || !user || !album || !file) {
+      throw new Error("No body");
+    }
+
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: "slingercode-test-1",
+        Key: `${user}/${album}/${file}`,
+      })
+    );
+
+    const base64 = await streamToBase64(response.Body);
+
+    res.writeHead(200, {
+      "Content-Type": "img/webp",
+      "Content-Length": base64.length,
+    });
+
+    return res.end(base64);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
